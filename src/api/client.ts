150360b9389bbf85c2299359d8cff2bf0892c129
Donnie Yu <dyu@bizflow.com>
2025-12-31 10:18:25 +0900

    refactor: link base url to see the environment variable

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 // 기본 Axios 인스턴스 설정
 export const client = axios.create({
-    baseURL: '/api', // Vite Proxy를 타게 됩니다.
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
-    timeout: 60000, // [Change] 기본 타임아웃을 10초 -> 60초로 증가
+    timeout: 60000,
 });
 
 // 응답 인터셉터 (에러 핸들링 용이성)
